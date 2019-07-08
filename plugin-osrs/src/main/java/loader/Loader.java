dbcb6c5fedee2da319d44efc01b6f7fd28081580
Fred Glass <glasshcs@gmail.com>
2019-07-08 19:38:45 +0100

    Moved CacheLibrary from local to maven dependency

---
+++ plugin-osrs/src/main/java/loader/Loader.java
@@ -0,0 +1,23 @@
+package loader;
+
+import api.CacheLoader;
+import org.displee.CacheLibrary;
+
+public class Loader implements CacheLoader {
+
+    public void loadItemDefinitions(CacheLibrary library) {
+
+    }
+
+    public void loadNpcDefintions(CacheLibrary library) {
+
+    }
+
+    public void loadSequences(CacheLibrary library) {
+
+    }
+
+    public void loadFrameArchive(int archiveId) {
+
+    }
+}
