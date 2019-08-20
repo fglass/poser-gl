56c00784b7080a6c7aa5b9ac83e44ae6e6303cce
Fred Glass <glasshcs@gmail.com>
2019-08-20 17:14:11 +0100

    Detect rotation gizmo intersection

--- app/src/main/kotlin/gizmo/RotationGizmo.kt
+++ app/src/main/kotlin/gizmo/RotationGizmo.kt
@@ -1,9 +1,7 @@
 package gizmo
 
 import model.Model
-import org.joml.Matrix4f
-import org.joml.Rayf
-import org.joml.Vector3f
+import org.joml.*
 import org.liquidengine.legui.style.color.ColorUtil
 import org.lwjgl.opengl.GL11.GL_TRIANGLES
 import org.lwjgl.opengl.GL11.glDrawArrays
@@ -14,7 +12,7 @@ import util.MatrixCreator
 
 class RotationGizmo(loader: Loader, private val shader: GizmoShader): Gizmo() {
 
-    private val scale = 30f
+    private val scale = 25f
     private val model: Model = getModel("rotation", loader)
     private val axes = arrayOf(
         GizmoAxis(AxisType.X, ColorUtil.fromInt(220, 14, 44, 1f), Vector3f(0f, 0f, 90f)),
@@ -25,6 +23,8 @@ class RotationGizmo(loader: Loader, private val shader: GizmoShader): Gizmo() {
     override fun render(context: RenderContext, viewMatrix: Matrix4f, ray: Rayf) {
         prepare(context, model, shader, viewMatrix)
 
+        selectAxis(getClosestAxis(ray))
+
         for (axis in axes) {
             val transformation = MatrixCreator.createTransformationMatrix(position, axis.rotation, scale)
             shader.loadTransformationMatrix(transformation)
@@ -32,6 +32,31 @@ class RotationGizmo(loader: Loader, private val shader: GizmoShader): Gizmo() {
             axis.colour.w = if (axis == selectedAxis) 0.6f else 1f // Lower opacity to indicate highlighted axis
             shader.loadColour(axis.colour)
             glDrawArrays(GL_TRIANGLES, 0, model.vertexCount)
+        }
+    }
+
+    private fun getClosestAxis(ray: Rayf): GizmoAxis? {
+        var minDistance = Float.MAX_VALUE
+        var closest: GizmoAxis? = null
+
+        for (axis in axes) {
+            val offset = Vector3f(scale).setComponent(axis.type.ordinal, 2f)
+            val min = Vector3f(position).sub(offset)
+            val max = Vector3f(position).add(offset)
+            val nearFar = Vector2f()
+
+            if (Intersectionf.intersectRayAab(ray, AABBf(min, max), nearFar) && nearFar.x < minDistance) {
+                minDistance = nearFar.x
+                closest = axis
+            }
+        }
+        return closest
+    }
+
+    private fun selectAxis(axis: GizmoAxis?) {
+        if (axis != selectedAxis) {
+            selectedAxis = axis
+            selectedAxis?.previousIntersection = Vector3f() // Reset intersection
         }
     }
 }
