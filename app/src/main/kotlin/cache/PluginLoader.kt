14b897f3b5a90fe61222376ef1ae04c6bac0abea
Fred Glass <glasshcs@gmail.com>
2019-10-12 18:35:25 +0100

    Rename plugin and api folders

--- app/src/main/kotlin/cache/PluginLoader.kt
+++ app/src/main/kotlin/cache/PluginLoader.kt
@@ -9,7 +9,7 @@ import java.util.ServiceLoader
 object PluginLoader {
 
     fun load(): Pair<List<ICacheLoader>, List<ICachePacker>> {
-        val path = File("../plugins")
+        val path = File("../plugin")
         val jars = path.listFiles { _, name -> name.endsWith(".jar") }?: return Pair(emptyList(), emptyList())
         val urls = jars.map { it.toURI().toURL() }
         val classLoader = URLClassLoader.newInstance(urls.toTypedArray(), Thread.currentThread().contextClassLoader)
