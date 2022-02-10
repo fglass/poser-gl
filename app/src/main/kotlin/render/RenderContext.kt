ca030cbc05bb7dc4d3ea836190ade4ad2126febc
Fred Glass <glasshcs@gmail.com>
2022-02-10 00:00:42 +0000

    feat(icon): update play and trash icons

--- app/src/main/kotlin/render/RenderContext.kt
+++ app/src/main/kotlin/render/RenderContext.kt
@@ -39,7 +39,7 @@ import java.lang.Boolean.TRUE
 import kotlin.system.exitProcess
 
 const val TITLE = "PoserGL"
-const val VERSION = "1.3.4"
+const val VERSION = "1.3.5"
 const val WIDTH = 800
 const val HEIGHT = 600
 
